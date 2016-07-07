162ca192956265ff09ebd4790c058fa2595205a6
hadoop <chljy33@naver.com>
2016-07-07 17:04:00 +0900

    new commit

---
+++ app/src/main/java/com/matcha/jjbros/matchaapp/FoodtruckMapActivity.java
@@ -0,0 +1,15 @@
+package com.matcha.jjbros.matchaapp;
+
+import android.os.Bundle;
+
+import com.nhn.android.maps.NMapActivity;
+
+/**
+ * Created by hadoop on 16. 7. 7.
+ */
+public class FoodtruckMapActivity extends NMapActivity {
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+    }
+}
