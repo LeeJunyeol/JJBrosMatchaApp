3db8f76c695f7a999aa704e1bf6fabf7c72640a5
mashiboa <mashiboa@gmail.com>
2016-07-09 22:31:13 +0900

    제일160709

---
+++ app/src/main/java/com/matcha/jjbros/matchaapp/user/UserMyinfoActivity.java
@@ -0,0 +1,16 @@
+package com.matcha.jjbros.matchaapp.user;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.matcha.jjbros.matchaapp.R;
+
+
+public class UserMyinfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_myinfo);
+    }
+}
