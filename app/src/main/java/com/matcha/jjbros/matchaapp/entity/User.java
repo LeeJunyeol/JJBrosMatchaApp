9a6e101d19709d1f804d3749fe4334eee5d0d20b
jylee <chljy33@naver.com>
2016-07-11 03:29:29 +0900

    사업자 / 사용자 회원가입 구축중

--- app/src/main/java/com/matcha/jjbros/matchaapp/entity/User.java
+++ app/src/main/java/com/matcha/jjbros/matchaapp/entity/User.java
@@ -1,25 +1,56 @@
 package com.matcha.jjbros.matchaapp.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.sql.Date;
 
 /**
  * Created by jylee on 2016-07-10.
  */
-public class User {
+public class User implements Parcelable {
     private String email;
     private String pw;
-    private boolean sex;
+    private Boolean sex;
     private Date birth;
 
-    public User() {
-    }
+    public User(){}
 
-    public User(String email, String pw, boolean sex, Date birth){
+    public User(String email, String pw, Boolean sex, Date birth){
         this.email = email;
         this.pw = pw;
         this.sex = sex;
         this.birth = birth;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.email);
+        dest.writeString(this.pw);
+        dest.writeValue(this.sex);
+        dest.writeValue(this.birth);
+    }
+
+    public User(Parcel in) {
+        this.email = in.readString();
+        this.pw = in.readString();
+        this.sex = Boolean.class.cast(in.readValue(Boolean.class.getClassLoader()));
+        this.birth = Date.class.cast(in.readValue(Date.class.getClassLoader()));
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+        public User[] newArray (int size) {
+            return new User[size];
+        }
+    };
 
     public String getEmail() {
         return email;
--- app/src/main/java/com/matcha/jjbros/matchaapp/owner/OwnerJoinActivity.java
+++ app/src/main/java/com/matcha/jjbros/matchaapp/owner/OwnerJoinActivity.java
@@ -1,18 +1,65 @@
 package com.matcha.jjbros.matchaapp.owner;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.matcha.jjbros.matchaapp.R;
+import com.matcha.jjbros.matchaapp.entity.User;
+import com.matcha.jjbros.matchaapp.main.LoginActivity;
 
 /**
  * Created by jylee on 2016-07-07.
  */
 public class OwnerJoinActivity extends AppCompatActivity {
 
+    private ImageView iv_foodtruck_photo;
+    private EditText et_foodtruck_name;
+    private EditText et_foodtruck_phone;
+    private EditText et_foodtruck_reg_num;
+    private Spinner sp_foodtruck_category;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_join);
+
+        User user = (User)getIntent().getParcelableExtra("user");
+
+        iv_foodtruck_photo = (ImageView)findViewById(R.id.iv_foodtruck_photo);
+        et_foodtruck_name = (EditText)findViewById(R.id.et_foodtruck_name);
+        et_foodtruck_phone = (EditText)findViewById(R.id.et_foodtruck_phone);
+        et_foodtruck_reg_num = (EditText)findViewById(R.id.et_foodtruck_reg_num);
+        sp_foodtruck_category = (Spinner)findViewById(R.id.sp_foodtruck_category);
+
+        Button btn_owner_submit = (Button)findViewById(R.id.btn_owner_submit);
+        btn_owner_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et_foodtruck_name.getText().toString();
+                int phone = Integer.parseInt(et_foodtruck_phone.getText().toString());
+                int reg_num = Integer.parseInt(et_foodtruck_reg_num.getText().toString());
+
+
+                Context context = getApplicationContext();
+                CharSequence text = "";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast;
+
+                text = "회원가입이 완료 되었습니다.";
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
