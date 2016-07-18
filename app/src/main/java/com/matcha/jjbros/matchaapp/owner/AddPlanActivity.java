483e40f7a385390a8d309e8af90f14556c35eb96
hadoop <chljy33@naver.com>
2016-07-18 20:56:51 +0900

    돌아돌아 제자리걸

--- app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
+++ app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
@@ -1,11 +1,13 @@
 package com.matcha.jjbros.matchaapp.owner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.ads.search.SearchAdView;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
@@ -14,6 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -23,6 +26,7 @@ import com.matcha.jjbros.matchaapp.R;
  * Created by hadoop on 16. 7. 13.
  */
 public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private SearchAdView mSearchAdView;
     private GoogleApiClient mGoogleApiClient;
 
     @Override
@@ -33,30 +37,23 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
         Toolbar tb_add_plan = (Toolbar) findViewById(R.id.tb_add_plan);
         setSupportActionBar(tb_add_plan);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
+/*
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+*/
 
-        int PLACE_PICKER_REQUEST = 1;
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
 
-        LatLngBounds newarkBounds = new LatLngBounds(
-                new LatLng(40.712216, -74.22655),       // South west corner
-                new LatLng(40.773941, -74.12544));      // North east corner
+        mapFragment.getMapAsync(this);
 
-
-        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
-        intentBuilder.setLatLngBounds(newarkBounds);
-        Context context = getApplicationContext();
-        startActivityForResult(intentBuilder.build(context), PLACE_PICKER_REQUEST);
     }
 
+/*
     @Override
     protected void onStart() {
         super.onStart();
@@ -68,14 +65,31 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
         mGoogleApiClient.disconnect();
         super.onStop();
     }
+*/
 
     @Override
     public void onMapReady(GoogleMap map) {
         LatLng sydney = new LatLng(-34, 151);
         map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        int PLACE_PICKER_REQUEST = 1;
+
+        LatLngBounds newarkBounds = new LatLngBounds(
+                new LatLng(40.712216, -74.22655),       // South west corner
+                new LatLng(40.773941, -74.12544));      // North east corner
+
+
+        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+        intentBuilder.setLatLngBounds(newarkBounds);
+        try{
+            startActivityForResult(intentBuilder.build(AddPlanActivity.this), PLACE_PICKER_REQUEST);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
+/*
     private GoogleApiClient setGoogleServiceBuilder(){
 
         GoogleApiClient mGoogleApiClientBuilder = new GoogleApiClient.Builder(this.getApplicationContext());
@@ -107,4 +121,5 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
         return mGoogleApiClientBuilder;
 
     }
+*/
 }
