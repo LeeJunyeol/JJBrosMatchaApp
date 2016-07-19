75372f3f55705da04bc6ba0010efcbe7c57cb506
hadoop <chljy33@naver.com>
2016-07-19 21:35:38 +0900

    지도 마커추가중 ^^

--- app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
+++ app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
@@ -1,33 +1,34 @@
 package com.matcha.jjbros.matchaapp.owner;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.search.SearchAdView;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.matcha.jjbros.matchaapp.R;
 
 /**
  * Created by hadoop on 16. 7. 13.
  */
-public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCallback{
-    private SearchAdView mSearchAdView;
-    private GoogleApiClient mGoogleApiClient;
+/*https://github.com/googlemaps/android-samples.git*/
+public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener{
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,89 +38,70 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
         Toolbar tb_add_plan = (Toolbar) findViewById(R.id.tb_add_plan);
         setSupportActionBar(tb_add_plan);
 
-/*
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-*/
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.plan_map);
         mapFragment.getMapAsync(this);
-
     }
-
-/*
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-*/
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng sydney = new LatLng(-34, 151);
-        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap = map;
 
-        int PLACE_PICKER_REQUEST = 1;
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+    }
 
-        LatLngBounds newarkBounds = new LatLngBounds(
-                new LatLng(40.712216, -74.22655),       // South west corner
-                new LatLng(40.773941, -74.12544));      // North east corner
+    @Override
+    public void onMapClick(LatLng latLng) {
 
+    }
 
-        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
-        intentBuilder.setLatLngBounds(newarkBounds);
-        try{
-            startActivityForResult(intentBuilder.build(AddPlanActivity.this), PLACE_PICKER_REQUEST);
-        } catch (Exception e){
-            e.printStackTrace();
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+    }
+
+    private Marker addMarkersToMap(LatLng latlng) {
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(latlng)
+                .title("Melbourne")
+                .snippet("Population: 4,137,400")
+                .draggable(true));
+        return marker;
+    }
+
+    public void onResetMap(View view){
+        if (!checkReady()) {
+            return;
         }
+        // Clear the map because we don't want duplicates of the markers.
+        mMap.clear();
     }
 
-/*
-    private GoogleApiClient setGoogleServiceBuilder(){
-
-        GoogleApiClient mGoogleApiClientBuilder = new GoogleApiClient.Builder(this.getApplicationContext());
-
-        //Places 서비스에 사용될 API 사용요청
-
-        mGoogleApiClientBuilder.addApi(Places.GEO_DATA_API);
-
-        mGoogleApiClientBuilder.addApi(Places.PLACE_DETECTION_API);
-
-        //Fused Location Provider 서비스에 사용될 API 사용요청
-
-        mGoogleApiClientBuilder.addApi(LocationServices.API);
-
-
-
-        CallbackConnectedGoogleService callbackConnectedGoogleService = new
-
-                CallbackConnectedGoogleService(this);
-
-        mGoogleApiClientBuilder.addConnectionCallbacks(callbackConnectedGoogleService);
-
-        mGoogleApiClientBuilder.addOnConnectionFailedListener(callbackConnectedGoogleService);
-
-        GoogleApiClient mGoogleApiClient = mGoogleApiClientBuilder.build();
-
-        mGoogleApiClient.connect();
-
-        return mGoogleApiClientBuilder;
-
+    private boolean checkReady() {
+        if (mMap == null) {
+            Toast.makeText(this, R.string.map_not_ready, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
-*/
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_plan, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mi_reset_plan_map){
+            onResetMap(findViewById(R.layout.activity_owner_add_plan));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
