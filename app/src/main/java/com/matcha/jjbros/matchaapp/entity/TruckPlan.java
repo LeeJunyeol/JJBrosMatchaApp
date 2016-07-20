1397037fb4216d35ab81d6ad76b11eb0471bf04f
hadoop <chljy33@naver.com>
2016-07-20 20:32:50 +0900

    ^.^

---
+++ app/src/main/java/com/matcha/jjbros/matchaapp/entity/TruckPlan.java
@@ -0,0 +1,8 @@
+package com.matcha.jjbros.matchaapp.entity;
+
+/**
+ * Created by hadoop on 16. 7. 20.
+ */
+public class TruckPlan {
+
+}
--- app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
+++ app/src/main/java/com/matcha/jjbros/matchaapp/owner/AddPlanActivity.java
@@ -1,22 +1,20 @@
 package com.matcha.jjbros.matchaapp.owner;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
-import com.google.android.gms.ads.search.SearchAdView;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,9 +24,12 @@ import com.matcha.jjbros.matchaapp.R;
  * Created by hadoop on 16. 7. 13.
  */
 /*https://github.com/googlemaps/android-samples.git*/
-public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener{
+public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener,
+        OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private double lat = 0.0;
+    private double lng = 0.0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,8 +39,7 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
         Toolbar tb_add_plan = (Toolbar) findViewById(R.id.tb_add_plan);
         setSupportActionBar(tb_add_plan);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.plan_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.plan_map);
         mapFragment.getMapAsync(this);
     }
 
@@ -49,36 +49,38 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
 
         mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
-                .title("Marker"));
+                .title("Marker"))
+                .setDraggable(true);
         mMap.setOnMapClickListener(this);
         mMap.setOnMapLongClickListener(this);
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
-
+        CheckBox cbx_input_mode = (CheckBox)findViewById(R.id.cbx_input_mode);
+        // 입력 모드일 경우,마커를 추가한다.
+        if(cbx_input_mode.isChecked()){
+            addMarkersToMap(latLng);
+        }
     }
 
     @Override
     public void onMapLongClick(LatLng latLng) {
+        addMarkersToMap(latLng);
+    }
 
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+
+        return false;
     }
 
     private Marker addMarkersToMap(LatLng latlng) {
         Marker marker = mMap.addMarker(new MarkerOptions()
-                .position(latlng)
-                .title("Melbourne")
-                .snippet("Population: 4,137,400")
-                .draggable(true));
-        return marker;
-    }
+                .position(latlng));
 
-    public void onResetMap(View view){
-        if (!checkReady()) {
-            return;
-        }
-        // Clear the map because we don't want duplicates of the markers.
-        mMap.clear();
+        return marker;
     }
 
     private boolean checkReady() {
@@ -100,7 +102,8 @@ public class AddPlanActivity extends AppCompatActivity implements OnMapReadyCall
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.mi_reset_plan_map){
-            onResetMap(findViewById(R.layout.activity_owner_add_plan));
+            if (checkReady())
+                mMap.clear();
         }
         return super.onOptionsItemSelected(item);
     }
